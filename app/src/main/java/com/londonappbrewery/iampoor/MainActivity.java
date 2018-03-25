9446aa51f0cf6bfd990c6bb773b72622218b3b08
Rehan <rehanraf@gmail.com>
2018-03-25 14:35:19 +0100

    added a comment

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Override //random comment
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
